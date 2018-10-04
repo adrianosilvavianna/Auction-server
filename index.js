6ccd81e7ef59b4fdae5a5dcf22a57eda090de811
Paulo César Teixeira <dev.paulocesar@gmail.com>
2018-10-03 21:59:46 -0300

    example app

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-const app = require('../src/app');
+const http = require('http');
+
+const hostname = '191.252.92.50';
+const port = 3002;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
